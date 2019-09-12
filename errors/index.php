45c3cbba3169c919eb70485a8cf00725c2b992b6
Salim <salim.anvarov@yahoo.com>
2019-09-12 09:50:45 -0400

    :lipstick: code formatting

--- errors/index.php
+++ errors/index.php
@@ -1,8 +1,5 @@
 <?php
-
 phpinfo();
-
 //error_reporting(0);
 // ini_set('display_errors', 'Off');
-
 $con = new PDO('mysql:host=127.0.0.1;dbname=sql_injectdion', 'root', ''); // will produce an error
