4db1655802d92b84cf51caca26988be921729f37
Salim <salim.anvarov@yahoo.com>
2019-09-12 09:50:19 -0400

    :lipstick: code formatting

--- http-cookie/index.php
+++ http-cookie/index.php
@@ -1,13 +1,5 @@
 <?php
-
-    $expires = new DateTime('+1 day');
-
-     setcookie('username', 'msanvarov', $expires->getTimestamp(), '/', null, null, true );
-
-    echo $_COOKIE['username'];
-
-
-
-
-
+$expires = new DateTime('+1 day');
+setcookie('username', 'msanvarov', $expires->getTimestamp(), '/', null, null, true);
+echo $_COOKIE['username'];
 ?>
