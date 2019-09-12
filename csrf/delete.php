9df9b865661bfa81620cdd2fe67454f1461d6f2f
Salim <salim.anvarov@yahoo.com>
2019-09-12 09:52:05 -0400

    :lipstick: code formatting

--- csrf/delete.php
+++ csrf/delete.php
@@ -1,29 +1,8 @@
 <?php
-
 require_once 'app/config.php';
-
-
-
-if($_SERVER['REQUEST_METHOD'] !== 'POST'){
-
-
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     die('SORRY CANNOT PERFORM THIS OPERATION');
-
-
 }
-
-
-
 $delete_user = $con->prepare("DELETE FROM users WHERE name = :name");
-
-
-$delete_user->execute([
-
-    'name' => $_SESSION['name']
-
-
-]);
-
-
-
+$delete_user->execute(['name' => $_SESSION['name']]);
 ?>
